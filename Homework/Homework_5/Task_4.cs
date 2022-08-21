e5a1c3a5db42a4b4f079443f7bbef5abfb28e453
iozzsab <iozz.sab@gmail.com>
2022-08-21 15:26:56 +0300

    commit homework5 + task4

--- Homework/Homework_5/Task_4.cs
+++ Homework/Homework_5/Task_4.cs
@@ -9,15 +9,73 @@
  * Требуется написать как можно более эффективную программу, которая будет выводить на экран фамилии и имена трёх худших по среднему баллу учеников. Если среди остальных есть ученики, набравшие тот же средний балл, что и один из трёх худших, следует вывести и их фамилии и имена.
  * */
 
+using Homework.Utils;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Homework_5
 {
     internal class Task_4
     {
+        public static void Main(string[] args)
+        {
+            Console.Title = "Best Worst";
+            OutputHelpers.PrintInfo(5, 4);
+            Console.WriteLine("Программа для анализа успеваемости учеников");
+            FindWorstStudents(AppDomain.CurrentDomain.BaseDirectory + "data_4.txt");
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Метод для выявления лучших и худших учеников по среднему баллу
+        /// </summary>
+        /// <param name="data_4">Список учеников</param>
+        /// <exception cref="Exception"></exception>
+        /// <exception cref="FormatException"></exception>
+        static void FindWorstStudents (string data_4)
+        {
+            using (var dataReader = new StreamReader("..\\..\\data_4.txt", encoding: System.Text.Encoding.GetEncoding(65001)))
+            {
+                if (!int.TryParse(dataReader.ReadLine(), out int counter) || !(counter >= 10 && counter <= 100))
+                    throw new Exception("Первая строка должна содержать цифровое значения о количестве учеников");
+
+                StringBuilder[] chart = new StringBuilder[29]; 
+                for (int i = 0; i < counter; i++)
+                {
+                    var parts = dataReader.ReadLine()?.Split(new[] { ' ' });
+                    if (parts == null) continue;
+                    var name = $"{parts[0]} {parts[1]}";
+                    if (!int.TryParse(parts[2], out int a) || !int.TryParse(parts[3], out int b) || !int.TryParse(parts[4], out int c))
+                        throw new FormatException("Arg_FormatException/Возникло исключение");
+                    int index = a + b + c - 1;
+                    if (chart[index] == null)
+                        chart[index] = new StringBuilder($"\n\t{name}");
+                    else
+                        chart[index].Append($"\n\t{name}");
+                }
+
+                Console.WriteLine("\nФамилии и имена худших по среднему баллу учеников: ");
+                int counterSt = 0;
+                for (int i = 6; i < chart.Length; i++) //начинаем с 6, для уменьшения количества итераций
+                {
+                    if (counterSt > 2)
+                        break;
+                    if (chart[i] == null) continue;
+                    Console.WriteLine($"\n{++counterSt} место - средний балл: {(float)(i + 1) / 3:F2}");
+                    Console.WriteLine(chart[i]);
+                }
+
+                Console.WriteLine("\nФамилии и имена лучших по среднему баллу учеников: ");
+                int counterStBetter = 0;
+                for (int i = 15; i < chart.Length; i--)
+                {
+                    if (counterStBetter > 2)
+                        break;
+                    if (chart[i] == null) continue;
+                    Console.WriteLine($"\n{++counterStBetter} место - средний балл: {(float)(i + 1) / 3:F2}");
+                    Console.WriteLine(chart[i]);
+                }
+            }
+        }
     }
 }
